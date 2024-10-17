f30915bbf211ca1be8528e0b3284bc20df214de3
Brian Michael <brian@cajunbug.net>
2024-10-17 08:38:21 -0400

    start.spring.io

---
+++ src/main/java/net/ttug/TtugApplication.java
@@ -0,0 +1,13 @@
+package net.ttug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ttug/TtugApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ttug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
